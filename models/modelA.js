e4c374ad4040c59c643d02b462ee3b4bc329d3b6
Elise Beall <elisebeall@gmail.com>
2021-04-23 19:31:08 -0600

    Initial commit

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airportObject = {
+  name: "Denver International Airport",
+  airportCode: "DEN",
+  yearOpened: 1995,
+  elevationInFeet: 5434,
+  isPublic: true,
+  isAccessibleByTrain: true,
+  hubForAirlines: ["Frontier Airlines" , "United Airlines"],
+  concourses: ["Concourse A" , "Concourse B" , "Concourse C"]
+}
