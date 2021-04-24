aba14f0a2a850299bfc9f70cddafd9e7e721d6fb
Elise Beall <elisebeall@gmail.com>
2021-04-23 19:17:12 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var famousPerson = "Mark Rober";
+var isActive = true;
+famousPerson = "Neil Armstrong";
+var num3 = 13;
+var num9 = 27;
+var sum = num3 + num9;
+var animals = ["squirrels" , "rabbits" , "pigs"];
+animals.shift();
+console.log(famousPerson.length);
