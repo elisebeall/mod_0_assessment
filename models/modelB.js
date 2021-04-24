2885a5eb42fa1ca3408bea801720dbb154226ee1
Elise Beall <elisebeall@gmail.com>
2021-04-23 19:44:43 -0600

    Initial commit

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var vehicle = {
+  brand: "Audi",
+  model: "S4 Avant",
+  year: 2002,
+  mileage: 113000,
+  isTurbo: true,
+  isAllWheelDrive: true,
+  dealerSpecs: ["Six-speed manual transmission" , "black leather interior" , "250 horsepower"],
+  uniqueSpecs: ["after-market wheels" , "blacked-out trim" , "upgraded soundsystem"]
+}
